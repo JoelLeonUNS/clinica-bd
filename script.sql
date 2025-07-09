cde49c98d1e68822186ed6c0acc078c6ff5f37d3
Joel Leon M <109642410+JoelLeonUNS@users.noreply.github.com>
2025-07-09 08:46:16 -0500

    Create script.sql

---
+++ script.sql
@@ -0,0 +1,32 @@
+CREATE DATABASE clinica;
+USE clinica;
+
+CREATE TABLE datos_basicos (
+  id INT AUTO_INCREMENT PRIMARY KEY,
+  enfermedades_previas VARCHAR(250),
+  observaciones VARCHAR(250)
+  );
+
+CREATE TABLE pacientes (
+  id INT AUTO_INCREMENT PRIMARY KEY,
+  datos_basicos_id INT NOT NULL,
+  nombres VARCHAR(100) NOT NULL,
+  fecha_nacimiento DATE NOT NULL,
+  edad INT GENERATED ALWAYS AS (TIMESTAMPDIFF(YEAR, fecha_nacimiento, CURDATE())) STORED,
+  telefonos JSON NOT NULL,
+  FOREIGN KEY datos_basicos_id REFERENCES datos_basicos(id)
+  );
+
+INSERT INTO datos_basicos ( enfermedades_previas, observaciones ) VALUES
+("Gastritis, Diabetes", "Tiene un alto grado de obesidad"),
+("Anemia", "Su grasa corporal es baja"),
+("Miopía", "Debe abtenerse a estar más 4 horas en el celular"),
+("Leucemia", "Debe tomar sesiones de quimioterapia"),
+("Diabetes", "Tiene un alto grado de obesidad");
+
+INSERT INTO pacientes ( datos_basicos_id, nombres, fecha_nacimiento, telefonos) VALUES
+(1, "Stuart Litle", "2000-10-15", JSON_ARRAY("960181410", "961191511")),
+(2, "Steve Jobs", "1988-01-11", JSON_ARRAY("910181410", "911191511")),
+(3, "Chris Evans", "2000-11-04", JSON_ARRAY("920181410", "921191511")),
+(4, "Peter Parker", "2000-08-25", JSON_ARRAY("930181410", "931191511")),
+(5, "Gwen Stacy", "2000-12-05", JSON_ARRAY("940181410", "941191511"));
