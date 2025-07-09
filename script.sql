adacf4b381a8c2b40de7b4fd561dac36fccfc458
Joel Leon M <109642410+JoelLeonUNS@users.noreply.github.com>
2025-07-09 08:57:20 -0500

    Update script.sql

--- script.sql
+++ script.sql
@@ -12,7 +12,7 @@ CREATE TABLE pacientes (
   datos_basicos_id INT NOT NULL,
   nombres VARCHAR(100) NOT NULL,
   fecha_nacimiento DATE NOT NULL,
-  edad INT GENERATED ALWAYS AS (TIMESTAMPDIFF(YEAR, fecha_nacimiento, CURDATE())) STORED,
+  edad INT,
   telefonos JSON NOT NULL,
   FOREIGN KEY (datos_basicos_id) REFERENCES datos_basicos(id)
   );
@@ -24,9 +24,9 @@ INSERT INTO datos_basicos ( enfermedades_previas, observaciones ) VALUES
 ("Leucemia", "Debe tomar sesiones de quimioterapia"),
 ("Diabetes", "Tiene un alto grado de obesidad");
 
-INSERT INTO pacientes ( datos_basicos_id, nombres, fecha_nacimiento, telefonos) VALUES
-(1, "Stuart Litle", "2000-10-15", JSON_ARRAY("960181410", "961191511")),
-(2, "Steve Jobs", "1988-01-11", JSON_ARRAY("910181410", "911191511")),
-(3, "Chris Evans", "2000-11-04", JSON_ARRAY("920181410", "921191511")),
-(4, "Peter Parker", "2000-08-25", JSON_ARRAY("930181410", "931191511")),
-(5, "Gwen Stacy", "2000-12-05", JSON_ARRAY("940181410", "941191511"));
+INSERT INTO pacientes ( datos_basicos_id, nombres, fecha_nacimiento, edad, telefonos) VALUES
+(1, "Stuart Litle", "2000-10-15", 25, JSON_ARRAY("960181410", "961191511")),
+(2, "Steve Jobs", "1988-01-11", 37, JSON_ARRAY("910181410", "911191511")),
+(3, "Chris Evans", "2000-11-04", 25, JSON_ARRAY("920181410", "921191511")),
+(4, "Peter Parker", "2000-08-25", 25, JSON_ARRAY("930181410", "931191511")),
+(5, "Gwen Stacy", "2000-12-05", 25, JSON_ARRAY("940181410", "941191511"));
